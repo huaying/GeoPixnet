168201145386765bac00c9fc172ef61c2e7bc099
huayingt <royal3501@hotmail.com>
2016-09-23 23:52:48 -0700

    1. customize map marker 2. optimize search result

--- routes/api.js
+++ routes/api.js
@@ -1,37 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var db = req.db;
-  var ne_lat = Number(req.query.ne_lat);
-  var ne_lng = Number(req.query.ne_lng);
-  var sw_lat = Number(req.query.sw_lat);
-  var sw_lng = Number(req.query.sw_lng);
-  var query = {
-    location: {
-      $geoWithin: {
-        $geometry: {
-          type: "Polygon",
-          coordinates: [
-            [[sw_lng, sw_lat ],[ne_lng, sw_lat ],[ne_lng, ne_lat ],[sw_lng, ne_lat ],[sw_lng, sw_lat ]]
-          ]
+  const db = req.db;
+  const ne_lat = Number(req.query.ne_lat);
+  const ne_lng = Number(req.query.ne_lng);
+  const sw_lat = Number(req.query.sw_lat);
+  const sw_lng = Number(req.query.sw_lng);
+  const query = {
+        location: {
+          $geoWithin: {
+            $box: [[sw_lng, sw_lat],[ne_lng, ne_lat]]
+          },
+        }
+      };
+  db.collection('scrapy_items2').find(query).count(function(err, total){
+    console.log(total)
+    const skip = Math.floor(Math.random() * ((total-200>0)?total-200: 0));
+    db.collection('scrapy_items2').find(query).skip(skip).limit(200).toArray(function(err,data){
+      if (err) throw err;
+      var stores = {};
+      var count = 0;
+      var key,i,article;
+      const ReturenNum = 30;
+
+      //if(data.length > ReturenNum) shuffle(data);
+      for(i=0; i<data.length; i++){
+        article = data[i];
+        key = JSON.stringify(article.location.coordinates);
+        if (key in stores) stores[key].push(article);
+        else {
+          if (count++ == ReturenNum)  break;
+          stores[key] = [article];
         }
       }
-    }
-  }
-  db.collection('scrapy_items2').find(query).toArray(function(err,data){
-    if (err) throw err;
-    var stores = {};
-    data.map(function(article){
-      var key = JSON.stringify(article.location.coordinates);
-      if (key in stores) stores[key].push(article);
-      else stores[key] = [article];
-    })
-    res.send(JSON.stringify(stores));
-    //res.send(JSON.stringify(data));
+      res.send(JSON.stringify(stores));
+    });
   });
-
 });
 
 module.exports = router;
