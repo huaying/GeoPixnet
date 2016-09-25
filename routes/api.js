1f6419d25b5fb2dafda6461edc4d05f8fb1c8c3e
huayingt <royal3501@hotmail.com>
2016-09-24 17:45:55 -0700

    get map_info cookie

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
-}
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const db = req.db;
@@ -19,15 +8,19 @@ router.get('/', function(req, res, next) {
   const ne_lng = Number(req.query.ne_lng);
   const sw_lat = Number(req.query.sw_lat);
   const sw_lng = Number(req.query.sw_lng);
+  const ne_lat_i = ne_lat + (sw_lat - ne_lat)*0.13;
+  const ne_lng_i = ne_lng + (sw_lng - ne_lng)*0.13;
+  const sw_lat_i = sw_lat - (sw_lat - ne_lat)*0.13;
+  const sw_lng_i = sw_lng - (sw_lng - ne_lng)*0.13;
   const query = {
         location: {
           $geoWithin: {
-            $box: [[sw_lng, sw_lat],[ne_lng, ne_lat]]
+            //$box: [[sw_lng, sw_lat],[ne_lng, ne_lat]]
+            $box: [[sw_lng_i, sw_lat_i],[ne_lng_i, ne_lat_i]]
           },
         }
       };
   db.collection('scrapy_items2').find(query).count(function(err, total){
-    console.log(total)
     const skip = Math.floor(Math.random() * ((total-200>0)?total-200: 0));
     db.collection('scrapy_items2').find(query).skip(skip).limit(200).toArray(function(err,data){
       if (err) throw err;
